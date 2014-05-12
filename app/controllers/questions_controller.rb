5cd264f81b6ac2362ddf79d66426bf8c68fbcb9f
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-12 12:20:14 -0500

    Working on ques 3 and 4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -26,24 +26,67 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    @most_movies = Director.find(Movie.sort.count.first.director.director_id
+
+  the_top_director = Director.new
+
+  Director.all.each do |director|
+
+    if
+
+    the_top_director.movies.count > director.movies.count
+
+    the_top_director = director
+
+end
+end
+
+    @director_with_the_most_movies = the_top_director.name
 
 
-    @director_with_the_most_movies = Director.find(@most_movies).name
-  end
+end
 
   def question_4
     # Which actor has been in the most movies on the list?
     # (If there's a tie, any one of them is fine)
 
-    # Your Ruby goes here.
+#     # Your Ruby goes here.
 
-    # @actor_movies = Actor.where
 
-    # @actor_with_the_most_movies = ???
-  end
+  the_actor = Actor.new
+
+  Actor.all.each do |actor|
+
+  if
+      the_actor.movies.count > actor.movies.count
+
+      the_actor = actor
+
+    end
+
+  @actor_with_the_most_movies = the_actor
+#   movie_counts = []
+
+#     Actor.all.each do |the_actor|
+#       number_of_movies_by_an_actor = the_actor.movies.count
+#       result = {:name => the_actor.name, :count => the_actor.movies.count}
+#       movie_counts.push(result)
+#     end
+# sorted_list = movies_counts.sort do |a,b|
+
+# b[:count] <=> a[:count]
+
+
+
+
+
+
+    # @actor_with_the_most_movies = sorted_list.first
+end
+end
 
   def question_5
+
+
     # This one is hard. Work on it after all your other review is complete.
 
     # Which actor/director pair has the most movies on the list?
